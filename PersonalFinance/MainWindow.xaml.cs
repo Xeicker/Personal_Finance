f5f31a788a0a2ba95a93de3da10e14c8632541cc
Edgar Aguilar <xeicker@gmail.com>
2022-01-29 14:21:22 -0600

    Improve main layout
    Introduce new sections and behaviours to make it more
    user friendly. Also move TagReader class to this file

--- PersonalFinance/MainWindow.xaml.cs
+++ PersonalFinance/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using PersonalFinance.Itemtemplates;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,117 +16,103 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Collections.ObjectModel;
-using PersonalFinance.Itemtemplates;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace PersonalFinance
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private bool Calculating= false;
+        private bool Calculating = false;
+        public decimal InvAmount { get; set; }
         public DateTime? RevenueFromDate
         {
             get => SharedFunctions.RevenueFromDate;
-            set
-            {
-                if (SharedFunctions.RevenueFromDate != value)
-                {
-                    SharedFunctions.RevenueFromDate = value;
-                    OnPropertyChanged();
-                }
-            }
+            set => SharedFunctions.RevenueFromDate = value;
         }
-        public DateTime? RevenueToDate
-        {
+        public DateTime? RevenueToDate {
             get => SharedFunctions.RevenueToDate;
-            set
-            {
-                if (SharedFunctions.RevenueToDate != value)
-                {
-                    SharedFunctions.RevenueToDate = value;
-                    OnPropertyChanged();
-                }
-            }
+            set => SharedFunctions.RevenueToDate = value;
         }
-        public decimal? OverallRevenue
-        {
-            get => SharedFunctions.OverallRevenue;
-            set
-            {
-                if (value != SharedFunctions.OverallRevenue)
-                {
-                    SharedFunctions.OverallRevenue = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        public decimal InvAmount { get; set; }
-       
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
         private async Task FillDataGrid(string cmdstr, DataGrid dtg)
         {
             string ConStr = ConfigurationManager.ConnectionStrings["FinanceDb"].ConnectionString;
-            using(SqlConnection con = new SqlConnection(ConStr))
-            {
-                SqlCommand cmd = new SqlCommand(cmdstr,con);
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable("Summary");
-                await Task.Run(()=>sda.Fill(dt));
-                dtg.ItemsSource = dt.DefaultView;
-            }
+            using SqlConnection con = new SqlConnection(ConStr);
+            SqlCommand cmd = new SqlCommand(cmdstr, con);
+            cmd.CommandTimeout = 120;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dt.Clear();
+            await Task.Run(() => sda.Fill(dt));
+            dtg.ItemsSource = dt.DefaultView;
         }
+        private void Label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xF7, 0xD7, 0x5F));
+        }
+
+        private void Label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Label).Background = SystemColors.ControlBrush;
+        }
+
         private void dtgMain_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
             if (e.PropertyType == typeof(System.DateTime))
                 (e.Column as DataGridTextColumn).Binding.StringFormat = "dd/MM/yyyy";
             else if (e.PropertyType == typeof(Decimal))
-                (e.Column as DataGridTextColumn).Binding.StringFormat = "C";
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "{0:$#,#.00;-$#,#.00}";
         }
-        private void Label_MouseEnter(object sender, MouseEventArgs e)
-        {
-            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(0xFF,0xF7,0xD7,0x5F));
-            
-        }
-        private void Label_MouseLeave(object sender, MouseEventArgs e)
-        {
-            (sender as Label).Background = SystemColors.ControlBrush;
-        }
+
         private async void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             string tag = (sender as Label).Tag.ToString();
-            await FillDataGrid(Queries.QueryManager[TagReader.GetParameter(tag, "query")],dtgMain);
+            await FillDataGrid(Queries.QueryManager[TagReader.GetParameter(tag, "query")], dtgMain);
         }
-        private void cmbInvest_DropDownOpened(object sender, EventArgs e)
+
+        private async void tabCUpdate_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            cmbInvest.ItemsSource = (App.Current.Resources["CValues"] as CValueCollection).Where(x=>x.Invest);
+            if(e.Source is TabControl)
+            {
+                if(e.AddedItems.Count>0 && e.AddedItems[0] is TabItem)
+                {
+                    var tab = e.AddedItems[0] as TabItem;
+                    switch (tab.Name)
+                    {
+                        case nameof(tabCValue):
+                        case nameof(tabIncome):
+                            await (((e.AddedItems[0] as TabItem)?.Content as UpdateDataWIthSingleDate)?.Initialize() ?? Task.CompletedTask);
+                            break;
+                        case nameof(tabInvest):
+                            await (App.Current.Resources["Investments"] as InvestmentCollection).Initialize();
+                            break;
+                    }
+                }
+            }
         }
+
+
         private async void btnUpdateInv_Click(object sender, RoutedEventArgs e)
         {
             var MainInvestment = (App.Current.Resources["MainInvestment"] as Investment);
             var Interests = (App.Current.Resources["Interests"] as InterestCollection);
             MainInvestment.InvAggregate = (cmbInvest.SelectedItem as CValue).AggID;
             MainInvestment.FromDate = dpkFromDate.SelectedDate;
-            MainInvestment.EndDate = (chbxEndDate.IsChecked??false)? dpkToDate.SelectedDate:null;
-            var dtg = dtgInterests;
+            MainInvestment.EndDate = (chbxEndDate.IsChecked ?? false) ? dpkToDate.SelectedDate : null;
             MainInvestment.InvAmount = InvAmount;
             int r = await MainInvestment.InsertToDB();
-            if (r > 0) {
+            await (dtg_Investmets.ItemsSource as InvestmentCollection).UpdateFromDB();
+            if (r > 0)
+            {
                 bool ins = await Interests.InsertToDB();
-                if (ins) {
+                if (ins || Interests.Count == 0)
+                {
                     Interests.Clear();
                     MessageBox.Show("Done", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
@@ -130,27 +120,50 @@ namespace PersonalFinance
             }
             MessageBox.Show("Something went wrong", "Fail", MessageBoxButton.OK, MessageBoxImage.Error);
         }
+
+        private void tbx_Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var collection = CollectionViewSource.GetDefaultView(dtg_Investmets.ItemsSource);
+            collection.Filter = x => (x as Investment).AggregateName?.Contains(tbx_Search.Text,StringComparison.OrdinalIgnoreCase)??false;
+            collection.Refresh();
+        }
+
         private async void btnCalculateRev_Click(object sender, RoutedEventArgs e)
         {
             if (Calculating)
                 return;
             Calculating = true;
-            
+
             var Dates = App.Current.Resources["Dates"] as DateCollection;
             var Revenues = App.Current.Resources["Revenues"] as RevenueCollection;
             var Rewards = App.Current.Resources["Rewards"] as CreditCardRewardCollection;
-            await (App.Current.Resources["CreditCards"] as CreditCardCollection).Initialize();
+            await(App.Current.Resources["CreditCards"] as CreditCardCollection).Initialize();
             await Rewards.Initialize();
             await Dates.Initialize();
             RevenueFromDate = Dates.MinElement(x => (decimal)Math.Abs((x.DateData - RevenueFromDate).Value.TotalDays)).DateData;
             RevenueToDate = Dates.MinElement(x => (decimal)Math.Abs((x.DateData - RevenueToDate).Value.TotalDays)).DateData;
             await Revenues.Update();
-            await (App.Current.Resources["Savings"] as CreditCardSavingsCollection).Update();
+            await(App.Current.Resources["Savings"] as CreditCardSavingsCollection).Update();
             Calculating = false;
         }
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+
+        private void cmbInvest_DropDownOpened(object sender, EventArgs e)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            var view = CollectionViewSource.GetDefaultView(this.cmbInvest.ItemsSource);
+            view.Filter = (x) => (x as CValue).Invest;
+            view.Refresh();
+        }
+    }
+    static class TagReader
+    {
+        public static string GetParameter(string tag, string key)
+        {
+            return tag.Split(';').Select(x =>
+            {
+                var a = x.Split('=');
+                return (key: a[0], value: a[1]);
+            })
+            .FirstOrDefault(x => x.key == key).value;
         }
     }
 }
